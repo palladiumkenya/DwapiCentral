3b81906369c5cc0facd6aae33accbe25a88fadea
meggmumbi <margaretmumbi16@gmail.com>
2023-05-01 22:24:34 +0300

    CT REPOSITORY

---
+++ src/shared/DwapiCentral.Shared/Application/Interfaces/Repository/Base/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DwapiCentral.Shared.Application.Interfaces.Repository.Base
+{
+    internal interface IRepository
+    {
+    }
+}
