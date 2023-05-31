8688df5af0662e6fea01bcf02f09e59065dc49c0
meggmumbi <margaretmumbi16@gmail.com>
2023-05-31 23:27:12 +0300

    fixed patients visits

--- src/ct/DwapiCentral.Ct.Infrastructure/Persistence/Repository/PatientStatusRepository.cs
+++ src/ct/DwapiCentral.Ct.Infrastructure/Persistence/Repository/PatientStatusRepository.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Z.Dapper.Plus;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace DwapiCentral.Ct.Infrastructure.Persistence.Repository
 {
@@ -22,25 +23,62 @@ namespace DwapiCentral.Ct.Infrastructure.Persistence.Repository
         }
         public Task MergeAsync(IEnumerable<PatientStatusExtract> patientStatusExtracts)
         {
+            
+
             var distinctExtracts = patientStatusExtracts
-               .GroupBy(e => new { e.PatientPk, e.SiteCode, e.TOVerifiedDate })
-               .Select(g => g.OrderByDescending(e => e.Id).First());
+            .GroupBy(e => new { e.PatientPk, e.SiteCode, e.ExitDate, e.ExitReason,e.TOVerifiedDate })
+            .Select(g => g.OrderByDescending(e => e.Id).First())
+            .ToList();
 
-            _context.Database.GetDbConnection().BulkMerge(distinctExtracts);
+            var existingExtracts = _context.PatientStatusExtracts
+                .AsEnumerable()
+                .Where(e => distinctExtracts.Any(d =>
+                    d.PatientPk == e.PatientPk &&
+                    d.SiteCode == e.SiteCode &&
+                    d.ExitDate == e.ExitDate &&
+                    d.ExitReason == e.ExitReason &&
+                    d.TOVerifiedDate == e.TOVerifiedDate))
+                .ToList();
 
-            var extractIdsToKeep = distinctExtracts.Select(e => e.Id).ToList();
-            var deleteQuery = $@"
-                    DELETE FROM PatientStatusExtracts
-                    WHERE Id NOT IN ({string.Join(",", extractIdsToKeep)})
-                ";
+            var distinctToInsert = distinctExtracts
+                .Where(d => !existingExtracts.Any(e =>
+                    d.PatientPk == e.PatientPk &&
+                    d.SiteCode == e.SiteCode &&
+                    d.ExitDate == e.ExitDate &&
+                    d.ExitReason == e.ExitReason &&
+                    d.TOVerifiedDate == e.TOVerifiedDate))
+                .ToList();
 
-            _context.Database.GetDbConnection().ExecuteAsync(deleteQuery);
+
+            _context.Database.GetDbConnection().BulkMerge(distinctToInsert);
+
+            //Perform deduplication after insertion
+            var duplicateIds = _context.PatientStatusExtracts
+                .AsEnumerable()
+                .GroupBy(e => new { e.PatientPk, e.SiteCode, e.ExitDate,e.ExitReason, e.TOVerifiedDate })
+                .Where(g => g.Count() > 1)
+                .SelectMany(g => g.Skip(1))
+                .Select(e => e.Id)
+                .ToList();
+
+            if (duplicateIds.Any())
+            {
 
 
-            _context.SaveChangesAsync();
+                var deleteQuery = $@"
+            DELETE FROM PatientVisitExtracts
+            WHERE Id IN ({string.Join(",", duplicateIds.Select(id => "'" + id.ToString() + "'"))})
+        ";
+
+
+                _context.Database.GetDbConnection().ExecuteAsync(deleteQuery);
+
+            }
+            
+            return Task.CompletedTask;
 
            
-            return Task.CompletedTask;
+           
         }
 
     }
--- src/ct/DwapiCentral.Ct.Infrastructure/Persistence/Repository/PatientVisitExtractRepository.cs
+++ src/ct/DwapiCentral.Ct.Infrastructure/Persistence/Repository/PatientVisitExtractRepository.cs
@@ -7,6 +7,7 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using Z.Dapper.Plus;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace DwapiCentral.Ct.Infrastructure.Persistence.Repository;
 
@@ -23,23 +24,55 @@ public class PatientVisitExtractRepository:IPatientVisitExtractRepository
 
     public Task MergeAsync(IEnumerable<PatientVisitExtract> patientVisitExtracts)
     {
-
         var distinctExtracts = patientVisitExtracts
-                 .GroupBy(e => new { e.PatientPk, e.SiteCode,e.VisitId, e.VisitDate })
-                 .Select(g => g.OrderByDescending(e => e.Id).First());
+        .GroupBy(e => new { e.PatientPk, e.SiteCode, e.VisitId, e.VisitDate })
+        .Select(g => g.OrderByDescending(e => e.Id).First())
+        .ToList();
 
-        _context.Database.GetDbConnection().BulkMerge(distinctExtracts);
+        var existingExtracts = _context.PatientVisitExtracts
+            .AsEnumerable() 
+            .Where(e => distinctExtracts.Any(d =>
+                d.PatientPk == e.PatientPk &&
+                d.SiteCode == e.SiteCode &&
+                d.VisitId == e.VisitId &&
+                d.VisitDate == e.VisitDate))
+            .ToList();
 
-        var extractIdsToKeep = distinctExtracts.Select(e => e.Id).ToList();
-        var deleteQuery = $@"
-                    DELETE FROM PatientVisitExtracts
-                    WHERE Id NOT IN ({string.Join(",", extractIdsToKeep)})
-                ";
+        var distinctToInsert = distinctExtracts
+            .Where(d => !existingExtracts.Any(e =>
+                d.PatientPk == e.PatientPk &&
+                d.SiteCode == e.SiteCode &&
+                d.VisitId == e.VisitId &&
+                d.VisitDate == e.VisitDate))
+            .ToList();
 
-        _context.Database.GetDbConnection().ExecuteAsync(deleteQuery);
+     
 
+           _context.Database.GetDbConnection().BulkMerge(distinctToInsert);
+
+        // Perform deduplication after insertion
+        //var duplicateIds = _context.PatientVisitExtracts
+        //    .GroupBy(e => new { e.PatientPk, e.SiteCode, e.VisitId, e.VisitDate })
+        //    .Where(g => g.Count() > 1)
+        //    .SelectMany(g => g.Skip(1))
+        //    .Select(e => e.Id)
+        //    .ToList();
+
+        //if (duplicateIds.Any())
+        //{
+        //    using (var connection = _context.Database.GetDbConnection())
+        //    {
+        //        var deleteQuery = $@"
+        //    DELETE FROM PatientVisitExtracts
+        //    WHERE Id IN ({string.Join(",", duplicateIds)})
+        //    ";
+
+        //        connection.ExecuteAsync(deleteQuery);
+        //    }
+        //}
 
         _context.SaveChangesAsync();
+        
         return Task.CompletedTask;
 
     }
