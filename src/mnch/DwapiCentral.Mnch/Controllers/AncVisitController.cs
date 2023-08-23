6dcb48d342a0a0da9b09c1c6665a381c49c5b1ba
meggmumbi <margaretmumbi16@gmail.com>
2023-08-23 16:39:17 +0300

    Mnch Endpoints

---
+++ src/mnch/DwapiCentral.Mnch/Controllers/AncVisitController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DwapiCentral.Mnch.Controllers
+{
+    public class AncVisitController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/mnch/DwapiCentral.Mnch/Controllers/MotherBabyPairController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DwapiCentral.Mnch.Controllers
+{
+    public class MotherBabyPairController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
